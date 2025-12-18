83d9c6e38bc317b3362cba935ae634dd35b29dc2
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-12-18 23:27:29 +0530

    add generate func and improved

--- tinyml/helpers.py
+++ tinyml/helpers.py
@@ -1,15 +1,21 @@
 import time
-
+import os
 
 
 def _timeit(func):
 	def enc_func(*args):
-		t = timeit(func, *args)
-		print(t)
+		I = int(os.environ.get("I", 10))
+		if I == -1:
+			while 1:
+				t = timeit(func, *args)
+				print(func.__name__, t)
+		else:		
+			for _ in range(I):
+				t = timeit(func, *args)
+				print(t)
+
 	return enc_func
 		
-		
-
 
 def timeit(func, *args):
 	st = time.monotonic()
@@ -19,3 +25,10 @@ def timeit(func, *args):
 	return et - st # in seconds
 
 
+def generate(lower=1, upper=100, size = (1, 1)):
+	import numpy as np
+	matrix = np.random.randint(lower, upper, size)
+	
+	return matrix
+	
+	
