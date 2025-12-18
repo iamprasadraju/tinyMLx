d84c9b09ae8328528456b7e066d6255f22e6fa2d
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-12-18 23:28:33 +0530

    removed try

--- tinyml/gemm.py
+++ tinyml/gemm.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 
 	A = [1, 0, 4, 3]    | 	B = [0, 1, 3]
@@ -19,17 +21,21 @@ def matmul(matrixA, matrixB):
 	
 	rowsB = matrixB.shape[0]
 	colsB = matrixB.shape[1]
-	
-	try: 
-		if colsA == rowsB:
-			matrixC = np.zeros((rowsA, colsB))
-			for i in range(rowsA):
-				for j in range(colsB):
-					acc = 0
-					for k in range(colsA):
-						acc += matrixA[i][k] * matrixB[k][j]
-					matrixC[i][j] = acc
-			return matrixC
+
+	if colsA == rowsB:
+		matrixC = np.zeros((rowsA, colsB))
+		for i in range(rowsA):
+			for j in range(colsB):
+				acc = 0
+				for k in range(colsA):
+					acc += matrixA[i][k] * matrixB[k][j]
+				matrixC[i][j] = acc
+		return matrixC
 			
 	else:
 		raise ValueError("no.of cols of A != no.of rows of B")
+
+
+def npmatmul(matrixA, matrixB):
+	return matrixA @ matrixB
+	
