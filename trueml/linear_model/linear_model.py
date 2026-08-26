13ccbf7b2aca81e18ccfd4f5b3ac8eb2be09b348
iamprasadraju <gls.prasadraju@gmail.com>
2026-08-26 14:19:18 +0530

    introducing the Tensor: numpy wrapper for now

--- trueml/linear_model/linear_model.py
+++ trueml/linear_model/linear_model.py
@@ -3,20 +3,24 @@ from __future__ import annotations
 import numpy as np
 
 from trueml.history import History
+from trueml.tensor import Tensor
 
 
 class LinearModel:
-    def __init__(self, lr=0.01, history=True):
+    def __init__(self, n_features, lr=0.01, history=True, uops=False):
         self.lr = lr
-        self.weights = None
-        self.bias = 0
+        self.uops = uops
+
+        self.weights = Tensor(np.random.random(n_features), uops=self.uops)
+        self.bias = Tensor(0.0, uops=self.uops)
         self.history = History() if history else None
 
-    def forward(self, X):
-        if self.weights is None:
-            self.weights = np.random.random((X.shape[1], 1))
-        self.X = X
-        return X @ self.weights + self.bias
+    def forward(self, X_train):
+        if not isinstance(X_train, Tensor):
+            X_train = Tensor(X_train, uops=self.uops)
+
+        self.X_train = X_train
+        return self.X_train @ self.weights + self.bias
 
     def backward(self, dL_dy_pred):
         """
@@ -35,8 +39,11 @@ class LinearModel:
 
         # dw = ∂L/∂w
         # db = ∂L/∂b
-        dw = self.X.T @ dL_dy_pred
+        dw = self.X_train.T @ dL_dy_pred
         db = np.sum(dL_dy_pred)
 
         self.weights -= self.lr * dw
         self.bias -= self.lr * db
+
+    def predict(self, X_test):
+        return X_test @ self.weights + self.bias
--- trueml/tensor.py
+++ trueml/tensor.py
@@ -4,19 +4,28 @@ from trueml._uops import UOp
 
 
 class Tensor:
-    def __init__(self, arr, uop=None):
+    def __init__(self, arr, uops=True):
         self.arr = np.asarray(arr)
         self.shape = self.arr.shape
         self.ndim = self.arr.ndim
         self.dtype = self.arr.dtype
+        self.uops = uops
 
-        if uop is None:
-            uop = UOp(op="LOAD", dtype=self.dtype, shape=self.shape, ndim=self.ndim)
-        self.uop = uop
+        if uops:
+            self.uop = UOp(
+                op="LOAD",
+                dtype=self.dtype,
+                shape=self.shape,
+                ndim=self.ndim,
+            )
 
     @classmethod
     def _make(cls, arr, op, *srcs):
         arr = np.asarray(arr)
+
+        if not any(src.uops for src in srcs):
+            return cls(arr, uops=False)
+        
         uop = UOp(
             op=op,
             dtype=arr.dtype,
@@ -24,8 +33,10 @@ class Tensor:
             ndim=arr.ndim,
             src=tuple(s.uop for s in srcs),
         )
-        return cls(arr, uop=uop)
-
+        tensor = cls(arr, uops=True)
+        tensor.uop = uop
+        return tensor
+    
     @property
     def metadata(self):
         return {
@@ -44,13 +55,23 @@ class Tensor:
 
         return Tensor._make(self.arr @ other.arr, "MATMUL", self, other)
 
+    
+    # TODO: add uops for array and scalar: int - array
+    def __rsub__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+
+        return Tensor._make(self.arr - other.arr, "SUB", self, other)
+            
+
     @property
     def T(self):
         return Tensor._make(self.arr.T, "TRANSPOSE", self)
-
+    """
     def __getattr__(self, attr):
         return getattr(self.arr, attr)
 
+    """
+    
     def __repr__(self):
         return (
             f"<Tensor {self.ndim}D shape={self.shape} dtype={self.dtype}>\n{self.arr}"
